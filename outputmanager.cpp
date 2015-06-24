531b3238bbeb5453aba623e850d29c4c25bc02a8
aurelien-rainone <aurelien@develer.com>
2015-06-24 15:57:19 +0200

    fix incorrect number of line jumps in output
    If a buffer ending with "\r\n" happens to be cut between '\r' and '\n'
    the 2 resulting QString will be interpreted as having 2 line jumps,
    instead of 1, when displayed in the output QTextEdit.
    To avoid this, final '\r' are removed from the received buffer,
    and prepended to the next one

--- outputmanager.cpp
+++ outputmanager.cpp
@@ -11,6 +11,9 @@
 
 #include "outputmanager.h"
 
+#include <algorithm>
+#include <iterator>
+
 OutputManager::OutputManager(QObject *parent) : QObject(parent)
 {
 
@@ -18,11 +21,40 @@ OutputManager::OutputManager(QObject *parent) : QObject(parent)
 
 void OutputManager::operator << (const QByteArray &data)
 {
-    // append raw data to the session buffer
+    // problem : if a buffer ending with "\r\n" happens to be cut between '\r' and '\n'
+    //           the 2 resulting QString will be interpreted as having 2 line jumps,
+    //           instead of 1, if displayed in a QTextEdit.
+    //           To avoid this, final '\r' are removed from the received buffer,
+    //           and prepended to the next one
+
+    // flag indicating that the previously received buffer ended with CR
+    static bool prev_ends_with_CR = false;
+
+    // append raw data to the buffer, untouched
     _buffer.append(data);
 
-    // notify that we have new data (in display format)
-    emit dataConverted(QString::fromLocal8Bit(data));
+    QString to_send;
+    if (prev_ends_with_CR)
+    {
+        // CR was removed at the previous buffer, so now we prepend it
+        to_send.append('\r');
+        prev_ends_with_CR = false;
+    }
+
+    if (data.size() > 0)
+    {
+        QByteArray::const_iterator end_cit = data.cend();
+        if (data.endsWith('\r'))
+        {
+            // if buffer ends with CR, we don't copy it
+            end_cit--;
+            prev_ends_with_CR = true;
+        }
+        std::copy(data.begin(), end_cit, std::back_inserter(to_send));
+    }
+
+    // notify that we have new data
+    emit dataConverted(to_send);
 }
 
 const QByteArray& OutputManager::buffer()
