b00411319f934a1110886beba77098aa04a29e6b
aurelien-rainone <aurelien@develer.com>
2015-10-08 13:04:19 +0200

    travis/linux update ppa to qt55

--- ci/travis/before_install-linux.sh
+++ ci/travis/before_install-linux.sh
@@ -4,8 +4,8 @@ echo
 echo Running before_install-linux.sh...
 echo
 
-sudo add-apt-repository --yes ppa:beineri/opt-qt541
+sudo add-apt-repository --yes ppa:beineri/opt-qt55
 sudo apt-get update -qq
-sudo apt-get install -qq qt54base qt54serialport qt54tools
-source /opt/qt54/bin/qt54-env.sh
+sudo apt-get install -qq qt55base qt55serialport qt55tools
+source /opt/qt55/bin/qt55-env.sh
 gem install fpm
