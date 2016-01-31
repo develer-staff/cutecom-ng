8e78633f5ef12c4af210d76c3f3f8e9856054070
Aurelien Rainone <aurelien-rainone@users.noreply.github.com>
2016-01-31 15:29:14 +0100

    Revert "Update to qt55 on CI build scripts"

--- ci/travis/script-osx.sh
+++ ci/travis/script-osx.sh
@@ -9,4 +9,4 @@ echo
 make
 /usr/local/opt/qt5/bin/macdeployqt bin/cutecom-ng.app -always-overwrite -verbose=2
 wget https://raw.githubusercontent.com/aurelien-rainone/macdeployqtfix/master/macdeployqtfix.py
-python macdeployqtfix.py bin/cutecom-ng.app/Contents/MacOS/cutecom-ng /usr/local/Cellar/qt5/5.5.0_1/
+python macdeployqtfix.py bin/cutecom-ng.app/Contents/MacOS/cutecom-ng /usr/local/Cellar/qt5/5.5.0/
